54e2545ed9c8796cf64883d0454e8b11b261d61a
satoshibaki <snm-792@mbox.re>
2020-10-08 16:53:01 +0900

    Initial Commit

---
+++ Single View App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Single View App
+//
+//  Created by 粕谷春菜 on 2020/10/08.
+//  Copyright © 2020 satoshi.nakamura4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
